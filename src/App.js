b99439c43315fc5b0022e9c3de9066d12e86d94f
agungtrue <syuhadadwiagung@gmail.com>
2021-03-31 16:09:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Syuhada Dwi Agungs
+          Syuhada Dwi Agung
         </p>
         <a
           className="App-link"
